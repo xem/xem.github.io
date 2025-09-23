4c6a54163203187c2756c617e35df70a81b2eb41
xem <maxime.euziere@gmail.com>
2025-09-23 17:45:23 +0200

    js13k25 wip

--- ui.js
+++ ui.js
@@ -10,6 +10,7 @@ menus = {
   
   articles: {
     intro: "Intro",
+    js13k25: "JS13kGames 2025",
     gamedevjs2025: "GamedevJS 2025",
     js13k24: "JS13kGames 2024",
     gamedevjs2024: "GamedevJS 2024",
